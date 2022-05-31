ea58b21fc4aab42f5cb4c944cc227927bd1f863c
Edwinleon95 <ridergaleon@yahoo.com>
2022-05-31 15:04:25 -0500

    new landinfg page responsive

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
-import './App.css';
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getGenres, getVideogames } from './redux/actions/actions';
 import { Route, Switch } from 'react-router-dom';
@@ -9,21 +8,19 @@ import VideogameDetail from './components/videogameDetail/VideogameDetail';
 import Create from './components/createVideogame/Create';
 function App() {
   const dispatch = useDispatch();
- 
 
-  useEffect(() =>{
+
+  useEffect(() => {
     dispatch(getGenres());
     dispatch(getVideogames());
   }, [dispatch]);
   return (
-    <div>
-      <Switch>
-        <Route exact path='/' component={LandinPage}/>
-        <Route exact path='/home' component={Home}/>
-        <Route exact path='/detail/:id' component={VideogameDetail}/>
-        <Route exact path='/create' component={Create}/>
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path='/' component={LandinPage} />
+      <Route exact path='/home' component={Home} />
+      <Route exact path='/detail/:id' component={VideogameDetail} />
+      <Route exact path='/create' component={Create} />
+    </Switch>
   );
 }
 
--- client/src/components/landingPage/LandingPage.jsx
+++ client/src/components/landingPage/LandingPage.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { IconController } from "../icons/IconController";
 import './landingPage.css'
 
 
 const LandinPage = () => {
     return (
-        <div className="container-landing">
-            <div className="container-btn">
-               <div className="title">
-                   <h1>PI Videojuegos</h1>
-                </div> 
-                <Link to='/home'>
-                  <div><button className="btn">COMENZAR</button></div> 
+        <div className="contenedor-landing">
+            <section className="contenedor-left">
+                <div className="title">
+                    <IconController />
+                    <h1>VIDEOGAMES</h1>
+                </div>
+                <h3>Welcome to the video game catalog, where
+                    There are titles from different years with their respective description, rating, release date and you can also see the platforms where it is available
+                </h3>
+                <Link to='/home' className="link">
+                    <button className="btn-link">Get Started </button>
                 </Link>
-            </div>
+            </section>
         </div>
     )
 }
