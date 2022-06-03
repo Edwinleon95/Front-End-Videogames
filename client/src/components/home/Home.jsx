b6743f291a74d2f4ce82bf41aadcbe655c81ba0a
Edwinleon95 <ridergaleon@yahoo.com>
2022-06-02 20:59:45 -0500

    Pagination styles and design responsive

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import Cards from "../cards/Cards";
 import './home.css'
 import { apiVideogames, changeState, clearStateFilters, clearVideogamesState, dbVideogames, filter, getVideogamesName, orderAscRating, orderDesRating, sortaz, sortza } from "../../redux/actions/actions";
-import Loading from "../loading/Loading";
 import NavBar from "../nav/NavBar";
 import Pagination from "../pagination/Pagination";
 import { Filters } from "../filters/Filters";
@@ -12,8 +10,7 @@ const Home = () => {
     const dispatch = useDispatch();
     let videogames = useSelector((state) => state.videogames)
 
-    const [cardsPerPage] = useState(15);
-    const [currentPage, setCurrentPage] = useState(1);
+
 
     const [overlay, setOverlay] = useState(false)
 
@@ -21,20 +18,7 @@ const Home = () => {
         setOverlay(!overlay)
     }
 
-    let lastCardInpage = currentPage * cardsPerPage;
-    let firstCardInpage = lastCardInpage - cardsPerPage
-    let totalPage = Math.ceil(videogames.length / cardsPerPage);
-    let currentCardsItems = videogames?.slice(firstCardInpage, lastCardInpage)
 
-    const onRightClick = () => {
-        const page = Math.max(currentPage + 1)
-        page > totalPage ? setCurrentPage(totalPage) : setCurrentPage(page)
-    }
-
-    const onLeftClick = () => {
-        const page = currentPage - 1
-        page < 1 ? setCurrentPage(1) : setCurrentPage(page)
-    }
 
     const onSearch = (name) => {
         dispatch(clearVideogamesState());
@@ -78,50 +62,38 @@ const Home = () => {
             dispatch(clearVideogamesState());
             dispatch(changeState());
             dispatch(clearStateFilters());
-            setCurrentPage(1);
         } else if (e.target.value === 'Videogames api') {
             dispatch(apiVideogames());
             dispatch(clearVideogamesState());
             dispatch(changeState());
             dispatch(clearStateFilters());
-            setCurrentPage(1);
         } else {
             dispatch(filter(videogames, e.target.value))
             dispatch(clearVideogamesState())
             dispatch(changeState())
             dispatch(clearStateFilters())
-            setCurrentPage(1);
         }
 
     }
 
     return (
-
-
-        <div className="home">
+        <>
             <NavBar openOverlay={openOverlay} />
-            {overlay ?
-                <Filters onSearch={onSearch}
-                    onChange={onChange}
-                    filter={filters}
-                    openOverlay={openOverlay}
-                /> : null}
 
-            <Pagination totalPage={totalPage}
-                currentPage={currentPage}
-                onLeftClick={onLeftClick}
-                onRightClick={onRightClick} />
-
-            {videogames.length ?
-                <Cards videogames={currentCardsItems}
-                    key={currentCardsItems.id}
-                /> : <Loading />}
-
-            <Pagination totalPage={totalPage}
-                currentPage={currentPage}
-                onLeftClick={onLeftClick}
-                onRightClick={onRightClick} />
-        </div>
+            {
+                overlay ?
+                    <Filters
+                        onSearch={onSearch}
+                        onChange={onChange}
+                        filter={filters}
+                        openOverlay={openOverlay} />
+                    : null
+            }
+            
+            <div className="home">
+                <Pagination />
+            </div>
+        </>
 
     )
 }
--- client/src/components/pagination/Pagination.jsx
+++ client/src/components/pagination/Pagination.jsx
@@ -1,15 +1,91 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import Cards from "../cards/Cards";
+import Loading from "../loading/Loading";
 import './pagination.css'
 
 
-const Pagination = (props) => {
-    const {currentPage,totalPage, onLeftClick, onRightClick}=props
+const Pagination = () => {
+
+    let videogames = useSelector((state) => state.videogames)
+    useEffect(() => {
+        setCurrentPage(1)
+    }, [videogames.length]);
+
+
+    const [cardsPerPage] = useState(15);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    let lastCardInpage = currentPage * cardsPerPage;
+    let firstCardInpage = lastCardInpage - cardsPerPage
+    let totalPage = Math.ceil(videogames.length / cardsPerPage);
+    let currentCardsItems = videogames?.slice(firstCardInpage, lastCardInpage)
+
+    const onRightClick = () => {
+        let page = currentPage + 1
+        page > totalPage ? setCurrentPage(totalPage) : setCurrentPage(page)
+    }
+
+    const onLeftClick = () => {
+        let page = currentPage - 1
+        page < 1 ? setCurrentPage(1) : setCurrentPage(page)
+    }
+
+    let pages = []
+    let page = 1
+    while (page <= totalPage) {
+        pages.push(page)
+        page++;
+    }
+
+    const actualPage = (event) => {
+        setCurrentPage(parseInt(event.target.id))
+    }
     return (
-        <div className="pagination">
-            <button onClick={onLeftClick}>⇐</button>
-            <div>{currentPage} de {totalPage}</div>
-            <button onClick={onRightClick}>⇒</button>
-        </div>
+        <>
+            {
+                videogames[0] === 'NotFound' || videogames.length <= 0 ?
+                    null :
+                    <div className="pagination">
+                        {
+                            currentPage !== 1 ?
+                                <button
+                                    className="button-prev"
+                                    onClick={onLeftClick}>Prev
+                                </button>
+                                : null
+                        }
+                        <section>
+                            {
+                                pages.map(e =>
+                                    <button
+                                    className={`btn-pages ${parseInt(e) === currentPage? 'active' : null}`}
+                                    onClick={actualPage}
+                                    id={e}>{e}
+                                    </button>)
+                            }
+                        </section>
+
+                        {
+                            currentPage !== totalPage ?
+                                <button
+                                    className="button-next"
+                                    onClick={onRightClick}>Next
+                                </button>
+                                : null
+                        }
+                    </div>
+            }
+
+            {
+                videogames.length ?
+                    <Cards
+                        videogames={currentCardsItems}
+                        key={currentCardsItems.id}
+                    />
+                    : <Loading />
+            }
+        </>
     )
 }
 
