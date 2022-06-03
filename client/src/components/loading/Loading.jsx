f948f1d7f8aa9c62f616e88cb14c6432a19b7335
Edwinleon95 <ridergaleon@yahoo.com>
2022-06-03 14:36:24 -0500

    Loading new design and responsive

--- client/src/components/loading/Loading.jsx
+++ client/src/components/loading/Loading.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { IconController } from "../icons/IconController";
 import './loading.css';
 
 
 const Loading = () => {
+
     return (
         <div className="container-loading">
+            <IconController/>
+            <div className="loading">
+                <h2><span>|</span></h2>
+            </div>
         </div>
     )
 }
